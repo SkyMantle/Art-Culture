ed610d0ab9c046c5140b8a3b53a44bc772dc5eff
Kostiantyn Kubriak <konstantin.kubriak@gmail.com>
2025-04-16 22:36:33 +0300

    Видалено .DS_Store, оновлено .gitignore, додано App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Art & Culture SSR</h1>
+      <p>Це стартова сторінка React App, підключена до SSR.</p>
+    </div>
+  );
+}
+
