33c452b5002a962f54688b7d7de6a12d2625a4db
Костянтин Кубряк <konstantin.kubriak@gmail.com>
2025-06-04 02:39:54 +0300

    Add createRoot import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'leaflet/dist/leaflet.css'
 import React from 'react'
+import { createRoot } from 'react-dom/client'
 import './styles/index.scss'
 import App from '/src/assets/screens/home/Home.jsx'
 import '/src/i18n/config'
