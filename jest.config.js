6b8a9c42801c0476fca2e6a4e2f463d0aa547082
Костянтин Кубряк <konstantin.kubriak@gmail.com>
2025-06-06 11:55:11 +0300

    chore(jest): add esm support

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 export default {
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': ['ts-jest', { useESM: true }],
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
 };
