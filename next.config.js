a216defd7526a312d5f1ca2584c37682dd600025
Костянтин Кубряк <konstantin.kubriak@gmail.com>
2025-06-07 02:08:10 +0300

    Allow images from remote domain

--- next.config.js
+++ next.config.js
@@ -1,2 +1,13 @@
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'art.playukraine.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
 export default config;
