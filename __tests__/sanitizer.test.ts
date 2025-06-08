81ba64950b9cd6131f19644e0e7bdb90c56e94d7
Костянтин Кубряк <konstantin.kubriak@gmail.com>
2025-06-08 14:12:08 +0300

    replace isomorphic dompurify

--- __tests__/sanitizer.test.ts
+++ __tests__/sanitizer.test.ts
@@ -1,4 +1,8 @@
-import DOMPurify from 'isomorphic-dompurify'
+import createDOMPurify from 'dompurify'
+import { JSDOM } from 'jsdom'
+const window = new JSDOM('').window
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const DOMPurify = createDOMPurify(window as any)
 
 /** Basic sanitization check */
 test('unsafe HTML is sanitized', () => {
