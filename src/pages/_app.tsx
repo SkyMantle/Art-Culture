edc74375dc898ed67086e7865a6c16011846149d
Костянтин Кубряк <konstantin.kubriak@gmail.com>
2025-06-08 04:11:18 +0300

    Fix intl provider import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from 'next/app'
-import { NextIntlProvider } from 'next-intl'
+import { NextIntlClientProvider } from 'next-intl'
 
 export default function App({ Component, pageProps }: AppProps<{ locale: string }>) {
   const { locale = 'uk', ...rest } = pageProps
   const messages = require(`../../messages/${locale}.json`)
   return (
-    <NextIntlProvider locale={locale} messages={messages}>
+    <NextIntlClientProvider locale={locale} messages={messages}>
       <Component {...rest} />
-    </NextIntlProvider>
+    </NextIntlClientProvider>
   )
 }
